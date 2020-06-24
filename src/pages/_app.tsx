4a884cfd577cd5d511c026b744afef7dfb841f07
HelipengTony <he@holptech.com>
2020-06-24 15:35:12 +0800

    Chore(design): add zeit-ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import Header from "./components/header";
+
+// 引入 Zeit-UI React
+import { ZeitProvider, CssBaseline } from "@zeit-ui/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ZeitProvider>
+      <CssBaseline />
+      <div>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </ZeitProvider>
+  );
+}
+
+export default MyApp;
