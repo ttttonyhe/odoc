ee8992ee9783a302c4958ac3f2def1dfef3cab96
HelipengTony <he@holptech.com>
2020-06-29 09:19:07 +0800

    update branch example

--- example/next.config.js
+++
@@ -1,9 +0,0 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-})
-
-module.exports = withMDX({
-    pageExtensions: ['mdx', 'tsx', 'ts', 'md'],
-    poweredByHeader: false,
-    generateEtags: false,
-})
