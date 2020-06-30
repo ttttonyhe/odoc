b15b96c2745ceeb768b97b32974bb9d56dedd364
HelipengTony <he@holptech.com>
2020-06-30 20:39:00 +0800

    feat(i18n): add support to i18n

--- create-odoc-site/index.js
+++ create-odoc-site/index.js
@@ -31,10 +31,28 @@ const execa = require("execa");
             message: "Please enter your github repo. branch(default will be master)",
             initial: "master"
         },
+        {
+            type: "text",
+            name: "siteDes",
+            message: "Please enter your site description",
+            initial: "One-click-away Documentation"
+        },
+        {
+            type: "text",
+            name: "siteKeywords",
+            message: "Please enter your site keywords (separate using ',')",
+            initial: "docs,documentation"
+        },
+        {
+            type: "text",
+            name: "siteI18n",
+            message: "Do you want to enable i18n? (enter 'true' or 'false')",
+            initial: "true"
+        },
     ])
 
     new Listr([{
-            title: 'Cloning ODoc example',
+            title: 'Basic structure generation',
             task: async () => {
                 await odocRepo('HelipengTony/odoc', '.', 'example')
             }
@@ -49,12 +67,69 @@ const execa = require("execa");
             title: 'Configuration file generation',
             task: async () => {
                 const configContent = `
-                    export default {
+                    module.exports = {
                         siteName: "${siteInfo.siteName}",
                         onlineSiteUrl: "${siteInfo.siteUrl}",
                         githubRepo: {
                             name: "${siteInfo.githubRepoName}",
                             branch: "${siteInfo.githubRepoBranch}"
+                        },
+                        siteDes: "${siteInfo.siteDes}",
+                        siteKeywords: "${siteInfo.siteKeywords}",
+                        i18nEnable: "${siteInfo.siteI18n}",
+                        i18nConfig: {
+                            "default": "English",
+                            "English": "/posts/en",
+                            "Chinese": "/posts/cn"
+                        },
+                        i18nLangs: ["English", "Chinese"],
+                        i18nLangConfig: {
+                            "English": {
+                                footer: "Edit this page on",
+                                date: "last update",
+                                sidebar: {
+                                    index: "Home",
+                                    search: {
+                                        title: "search",
+                                        placeholder: "Search..."
+                                    },
+                                    share: {
+                                        title: "Share",
+                                        url: "Copy URL",
+                                        qr: "QR Code",
+                                        fb: "Facebook",
+                                        tw: "Twitter"
+                                    },
+                                    source: {
+                                        title: "Source",
+                                        repo: "Repository"
+                                    },
+                                    mobileIndex: "Click to view index"
+                                }
+                            },
+                            "Chinese": {
+                                footer: "参与此页面的编辑",
+                                date: "最后更新",
+                                sidebar: {
+                                    index: "文档首页",
+                                    search: {
+                                        title: "页面搜索",
+                                        placeholder: "关键词..."
+                                    },
+                                    share: {
+                                        title: "分享页面",
+                                        url: "复制链接",
+                                        qr: "二维码图",
+                                        fb: "脸书分享",
+                                        tw: "推特分享"
+                                    },
+                                    source: {
+                                        title: "内容仓库",
+                                        repo: "Github 仓库"
+                                    },
+                                    mobileIndex: "展开页面引索"
+                                }
+                            }
                         }
                     }
                 `;
@@ -62,6 +137,9 @@ const execa = require("execa");
                     if (err) {
                         console.error('Generation failed');
                     } else {
+                        if (siteInfo.siteI18n) {
+                            console.log("You have enabled i18n, please edit 'odoc.config.js' to customize translation");
+                        }
                         console.log("ODoc setup has completed. Please wait...");
                         return;
                     }
--- odoc/generators/menu.js
+++ odoc/generators/menu.js
@@ -10,28 +10,61 @@ exports.__esModule = true;
 var fs = require("fs");
 var resolve = require("path").resolve;
 var join = require("path").join;
+var odocConfig = require("../../odoc.config");
 // 获取当前项目路径
 var catePath = resolve("../../") + "/src/pages/posts";
 // 获取当前项目排除路径
 var cateExcludePath = resolve("../../") + "/src/pages";
 // 初始化内容数组
 var catesArray = [];
-// 获取路径下所有文件数组
-var cates = fs.readdirSync(catePath);
-// 遍历文件数组
-cates.forEach(function (item) {
-    // 获取当前文件路劲
-    var cPath = join(catePath, item);
-    // 获取当前文件数据
-    var stat = fs.statSync(cPath);
-    // 判断是否为文件夹
-    if (stat.isDirectory() === true) {
-        catesArray.push({
-            cateName: cPath.replace(catePath + "/", ""),
-            catePath: cPath.replace(cateExcludePath, "")
+// i18n 开启时再深入一级目录来获取分类
+if (odocConfig.i18nEnable) {
+    var getFileFunction_1 = function (path, count) {
+        var eachCount = 0;
+        // 获取路径下所有文件数组
+        var cates = fs.readdirSync(path);
+        // 遍历文件数组
+        cates.forEach(function (item) {
+            // 获取当前文件路劲
+            var cPath = join(path, item);
+            // 获取当前文件数据
+            var stat = fs.statSync(cPath);
+            // 判断是否为文件夹
+            if (stat.isDirectory() === true && !count) {
+                // 递归一次
+                getFileFunction_1(cPath, 1);
+            }
+            else {
+                var fileName = cPath.split("/")[cPath.split("/").length - 1];
+                if (fileName !== "index.mdx" && fileName.charAt(0) !== ".")
+                    catesArray.push({
+                        cateName: cPath.replace(catePath + "/", ""),
+                        catePath: cPath.replace(cateExcludePath, "")
+                    });
+            }
         });
-    }
-});
+        return eachCount;
+    };
+    getFileFunction_1(catePath, 0);
+}
+else {
+    // 获取路径下所有文件数组
+    var cates = fs.readdirSync(catePath);
+    // 遍历文件数组
+    cates.forEach(function (item) {
+        // 获取当前文件路劲
+        var cPath = join(catePath, item);
+        // 获取当前文件数据
+        var stat = fs.statSync(cPath);
+        // 判断是否为文件夹
+        if (stat.isDirectory() === true) {
+            catesArray.push({
+                cateName: cPath.replace(catePath + "/", ""),
+                catePath: cPath.replace(cateExcludePath, "")
+            });
+        }
+    });
+}
 var file = resolve("../../") + "/src/data/menuitems.json";
 //写入文件
 fs.writeFile(file, JSON.stringify(catesArray), function (err) {
