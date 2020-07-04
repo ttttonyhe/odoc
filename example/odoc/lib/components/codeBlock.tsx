d24a70f45f676518c5f5c6c5f00626fd132264b6
TonyHe <helipeng@MacBook-Pro.local>
2020-07-04 21:27:25 +0800

    fix(build): syntax highlighting error

--- example/odoc/lib/components/codeBlock.tsx
+++ example/odoc/lib/components/codeBlock.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import github from "prism-react-renderer/themes/oceanicNext";
 export default ({ children, className }) => {
-  const language = className.replace(/language-/, "");
+  const language = className ? className.replace(/language-/, "") : "default";
   return (
     <Highlight
       {...defaultProps}
@@ -11,7 +11,10 @@ export default ({ children, className }) => {
       theme={github}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: "20px 20px 0px 20px"}}>
+        <pre
+          className={className}
+          style={{ ...style, padding: "20px 20px 0px 20px" }}
+        >
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
